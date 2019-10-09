6e8f1f2550c49d8df1958cf7af53e29b218153e8
artinsepanta <faribasalehi@hotmail.com>
2019-10-08 19:15:58 -0500

    w6d2

--- routers/users.js
+++ routers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const usersController = require('../controllers/users')
 const router = express.Router()
 
+
 router.get('/', usersController.getAllUsers)
 
 router.get('/:id', usersController.getUserById)
